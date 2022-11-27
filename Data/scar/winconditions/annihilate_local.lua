8be73d6bc633141e0ede4c3f91c92c5f4a19cf91
Espresso <TestXBot@gmail.com>
2022-11-27 20:24:05 +0000

    Create annihilate_local.lua

---
+++ Data/scar/winconditions/annihilate_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+	exclusive				= false,
+	victory_condition	= true,
+	always_on			= false,
+	title						= "$60000",
+	win_message		= "$60100",
+	lose_message		= "$60200",
+	description			= "$60300"
+}
