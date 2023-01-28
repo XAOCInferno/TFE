342b7ca3b879f19e4731ed27019f34b87105f1cb
Espresso <TestXBot@gmail.com>
2023-01-28 14:37:58 +0000

    New Cover System Setup
    Simply setting up the cover system from the ME to the game. This commit is a good place to revert to if it doesn't work

--- Data/game/defterraintypes.lua
+++ Data/game/defterraintypes.lua
@@ -56,5 +56,54 @@ TerrainTypes =
 				{ "$45150", "$45151", "$45152" },	-- LIST
 			},
 		},
+		{	name="Height_High", tp = 6, colour = {0,170,180},
+			uiInfo = {
+				"$20040000",	-- NAME
+				"$20040001",	-- DESCRIPTION
+				{ "$20040002", "$20040003", "$20040004" },	-- LIST
+			},
+		},
+		{	name="Height_High_Light", tp = 7, colour = {140,255,255},
+			uiInfo = {
+				"20040006",	-- NAME
+				"$20040007",	-- DESCRIPTION
+				{ "$20040008", "$20040009", "$20040010" },	-- LIST
+			},
+		},
+		{	name="Height_High_Heavy", tp = 8, colour = {30,100,100},
+			uiInfo = {
+				"$20040012",	-- NAME
+				"$20040013",	-- DESCRIPTION
+				{ "$20040014", "$20040015", "$20040016" },	-- LIST
+			},
+		},
+		{	name="Height_Mid", tp = 9, colour = {40,55,240},
+			uiInfo = {
+				"$20040018",	-- NAME
+				"$20040019",	-- DESCRIPTION
+				{ "$20040020", "$20040021", "$20040022" },	-- LIST
+			},
+		},
+		{	name="Height_Mid_Light", tp = 10, colour = {130,140,255},
+			uiInfo = {
+				"$20040024",	-- NAME
+				"$20040025",	-- DESCRIPTION
+				{ "$20040026", "$20040027", "$20040028" },	-- LIST
+			},
+		},
+		{	name="Height_Mid_Heavy", tp = 11, colour = {10,20,100},
+			uiInfo = {
+				"$20040030",	-- NAME
+				"$20040031",	-- DESCRIPTION
+				{ "$20040032", "$20040033", "$20040034" },	-- LIST
+			},
+		},
+		{	name="Poison", tp = 12, colour = {0,90,0},
+			uiInfo = {
+				"$20040036",	-- NAME
+				"$20040037",	-- DESCRIPTION
+				{ "$20040038", "$20040039", "$20040040" },	-- LIST
+			},
+		},
 	},
 }
