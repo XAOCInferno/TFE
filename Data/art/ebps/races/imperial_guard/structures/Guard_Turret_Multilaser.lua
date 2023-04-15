80604c866737102ec3b336668d38b89dff9e8bfb
BenB <TestXBot@gmail.com>
2023-04-15 15:33:14 +0100

    guard multi las tur, noise blast abil fix, reimport rgd
    added multi laser turret to guard and fixed the noise blast ability (dk if for sure didn't test..) reimported rgd

---
+++ Data/art/ebps/races/imperial_guard/structures/Guard_Turret_Multilaser.lua
@@ -0,0 +1,22 @@
+simvis_attributes =
+{
+
+	selection_child			= 0.675,
+	selection_ground		= 0.675,
+
+	vis_scale_min			= 0.6,
+	vis_scale_max			= 0.6,
+	-- Shadow Types:
+	-- 0 = None
+	-- 1 = blob (rotated by light dir)
+	-- 2 = blob (rotated by object dir)
+	-- 3 = shadow map
+	-- 4 = stencil shadow volume
+	shadow_type			= 4,
+--	shadow_scale			= 0.0275
+
+	occluder = 1,
+        persistent_body = 0 ,
+	distance_game_mesh_lod = 80,
+}
+
