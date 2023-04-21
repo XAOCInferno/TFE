e5d681227b6c687fa174a525acd2532c0ca43073
BenB <TestXBot@gmail.com>
2023-04-21 23:36:34 +0100

    Improved debug, AL scars load immedietly
    Debug now shows mod name and world time. can disable it by turning off debug scar to improve performance
    AL scars are now in TFE_Immediate_Load and no longer have any delays
    AL Ban and Force now run immedietly so AL is set before load out of game
    Still need to find a solution for if we have both Force AL and Ban AL in a single game

---
+++ Data/scar/winconditions/TFEDebugMode_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = false,
+always_on = false,
+title = "TFE Enable Debug",
+win_message = "",
+lose_message = "",
+description = "Enable Debug Messages for 'The Final Expansion' mod."
+}
