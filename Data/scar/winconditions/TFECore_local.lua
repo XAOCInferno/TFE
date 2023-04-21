a5af073f1b41a456a97203fe25accf82fce152e7
BenB <TestXBot@gmail.com>
2023-04-21 21:08:02 +0100

    Replaced all reference of UBM to TFE in scripts
    Legacy mod name UBM replaced with new mod name TFE (The Final Expansion). Scripts should follow same logic. RIP UBM

--- Data/scar/winconditions/TFECore_local.lua
+++ Data/scar/winconditions/TFECore_local.lua
@@ -3,8 +3,8 @@ Localization =
 exclusive = false,
 victory_condition = false,
 always_on = true,
-title = "UBM",
+title = "TFE",
 win_message = "",
 lose_message = "",
-description = "Core code for UBM Mod."
+description = "Core code for 'The Final Expansion' mod."
 }
