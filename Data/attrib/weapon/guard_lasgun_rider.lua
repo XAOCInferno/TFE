bcd049408eac4ffc6e614b2e82fe153c3b091522
BenB <TestXBot@gmail.com>
2023-05-27 14:58:10 +0100

    rider rebalances
    some simple rider reblanaces, mainly nerfs...

--- Data/attrib/weapon/guard_lasgun_rider.lua
+++ Data/attrib/weapon/guard_lasgun_rider.lua
@@ -1,6 +1,6 @@
 GameData = Inherit([[]])
-GameData["accuracy"] = 0.45
-GameData["accuracy_reduction_when_moving"] = 0.349999994
+GameData["accuracy"] = 0.43
+GameData["accuracy_reduction_when_moving"] = 0.33
 GameData["area_effect"] = Reference([[tables\area_effect_table.lua]])
 GameData["area_effect"]["area_effect_information"]["remove_modifiers_with_source_entity"] = true 
 GameData["area_effect"]["area_effect_information"]["start_from_caster"] = false
@@ -63,10 +63,10 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"] = Reference([[type_armour\tp_air_med.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"]["screen_name_id"] = [[$4450037]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 14
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 12.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 11
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 0.8
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 2.1
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 0.7
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 2
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Guard\Weapons\Lasgun_Impact_Flesh]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry02"]["event"] = [[Guard\Weapons\Lasgun_Impact_bone]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry03"]["event"] = [[Guard\Weapons\Lasgun_Impact_light_metal]]
