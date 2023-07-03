c9bf5d4ec62a185cbac2552851c1c7ec419d0a44
BenB <TestXBot@gmail.com>
2023-07-03 16:18:40 +0100

    assassin rebalances
    improved stats, research is now addon, increased size for readability, improved weapons and removed t1 vs t3 weapons (now the same weapon)

---
+++ Data/art/ebps/races/imperial_guard/troops/vindicare_temple_assassin.lua
@@ -0,0 +1,19 @@
+simvis_attributes =
+{
+	vis_scale_min = 1.1, -- where x.xx is a scaling factor. eg. 1.50 = %150
+	vis_scale_max = 1.1,
+	
+	selection_volume		= 1,
+	selection_ground		= 1,
+
+	-- Shadow Types:
+	-- 0 = None
+	-- 1 = blob (rotated by light dir)
+	-- 2 = blob (rotated by object dir)
+	-- 3 = shadow map
+	-- 4 = stencil shadow volume
+	shadow_type			= 1,
+	shadow_scale			= 0.0195,
+	
+	occludee	= 1,
+}
