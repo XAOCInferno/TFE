b85ac9673b75b1ea95d63f9fe22fd313a6a7d8ca
BenB <TestXBot@gmail.com>
2023-08-28 15:07:02 +0100

    fix for warpfire sacrifice not affecting enemies
    made the child through summoned entities not from the caster

---
+++ Data/attrib/ebps/environment/gameplay/chaos_cultist_warpfire_caster.lua
@@ -0,0 +1,9 @@
+GameData = Inherit([[]])
+GameData["ability_ext"] = Reference([[ebpextensions\ability_ext.lua]])
+GameData["ability_ext"]["abilities"]["ability_01"] = [[abilities\chaos_cultist_champion_warpfire_sacrifice2.lua]]
+GameData["entity_blueprint_ext"] = Reference([[ebpextensions\entity_blueprint_ext.lua]])
+GameData["entity_blueprint_ext"]["animator"] = [[Environment\Necron\solar_pulse]]
+GameData["sim_entity_ext"] = Reference([[ebpextensions\sim_entity_ext.lua]])
+GameData["suicide_ext"] = Reference([[ebpextensions\suicide_ext.lua]])
+GameData["suicide_ext"]["lifetime"] = 3
+GameData["type_ext"] = Reference([[ebpextensions\type_ext.lua]])
