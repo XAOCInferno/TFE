32ae42d1828f4d4b5dc2581d99b21131da63821a
BenB <TestXBot@gmail.com>
2023-09-19 23:30:41 +0100

    Further work on the valkyrie

--- Data/attrib/ebps/races/world/troops/world_global_ability_caster_guard_race.lua
+++ Data/attrib/ebps/races/world/troops/world_global_ability_caster_guard_race.lua
@@ -1,6 +1,7 @@
 GameData = Inherit([[]])
 GameData["ability_ext"] = Reference([[ebpextensions\ability_ext.lua]])
 GameData["ability_ext"]["abilities"]["ability_01"] = [[abilities\global_guard_summon_bunker.lua]]
+GameData["ability_ext"]["abilities"]["ability_04"] = [[abilities\global_guard_valkyrie_reinforce.lua]]
 GameData["cost_ext"] = Reference([[ebpextensions\cost_ext.lua]])
 GameData["cost_ext"]["time_cost"]["cost"]["faith"] = 0
 GameData["cost_ext"]["time_cost"]["cost"]["requisition"] = 0
