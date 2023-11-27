1404cd8ad9e33184177fefc0dcdce43132298188
Benjamin Bertolli <TestXBot@gmail.com>
2023-11-27 13:55:09 +0000

    [ELD] Rebalances to warlock smite, emphasis on central area so precise aim (Requires export to RGD)

--- Data/attrib/abilities/eldar_smite_warlock2.lua
+++ Data/attrib/abilities/eldar_smite_warlock2.lua
@@ -63,10 +63,10 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_12"]["armour_type"] = Reference([[type_armour\tp_infantry_low.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"] = Reference([[type_armour\tp_air_med.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 50
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 35
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 2
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 2
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 70
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 55
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 15
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 35
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry05"] = Reference([[tables\hit_event_entry.lua]])
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry05"]["surface"] = Reference([[type_surface\tp_monolith.lua]])
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_01"]["modifier"]["shield_of_faith"] = false
