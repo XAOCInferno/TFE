9fdaf3d0e4dcfd9740b8dcfc53218b3255a91572
Benjamin Bertolli <TestXBot@gmail.com>
2023-11-30 13:57:46 +0000

    [CODE] Added stacktrace to most of Subscars (still need to add to individual global abilities)

--- Data/scar/TFE_Core/Subscars/Resources/AutoGenResources/PassiveResourceGeneration_Settings.lua
+++ Data/scar/TFE_Core/Subscars/Resources/AutoGenResources/PassiveResourceGeneration_Settings.lua
@@ -3,6 +3,8 @@ debug_PassiveResourceGeneration_Settings_FileName = "PassiveResourceGeneration_S
 SpecificRaceResourceGenerationDetails = {}
 function SpecificRaceResourceGenerationDetails:new (_RaceName, _HasPassiveResGen, _RT_Requisition, _RT_Power, _RT_Pop, _RT_Souls, _RT_Faith)
 	
+	AddStacktrace("SpecificRaceResourceGenerationDetails:new")
+
 	Log(eLOG_TYPE_INFO, eLOG_FULL, debug_PassiveResourceGeneration_Settings_FileName, "SpecificRaceResourceGenerationDetails:Constructor",  "Importing race: '".._RaceName.."'...")
 	
     local o = 
@@ -37,6 +39,8 @@ function SpecificRaceResourceGenerationDetails:new (_RaceName, _HasPassiveResGen
 	
 	Log(eLOG_TYPE_INFO, eLOG_FULL, debug_PassiveResourceGeneration_Settings_FileName, "SpecificRaceResourceGenerationDetails:Constructor",  "Importing race: '".._RaceName.."' Success!")
 	
+	RemoveStacktrace()
+
     return setmetatable(o, self)
 
 end
@@ -44,78 +48,81 @@ end
 g_AllRaceResourcesSetup = {}
 g_FactionSpecificResourceGeneration = {	}
 function Setup_PassiveResourceGeneration_Settings(_PlayerRace)
+	
+	AddStacktrace("Setup_PassiveResourceGeneration_Settings")
+	
+	--Should this if statement be here or elsewhere? it encapsulates the whole function.
+	if(TableContains(g_AllRaceResourcesSetup, _PlayerRace) == false) then
 
-	if(TableContains(g_AllRaceResourcesSetup, _PlayerRace)) then
-
-		return 
+		import('Upkeep/races/'.._PlayerRace..'.scar')
 		
+		--This code is vulnerable, players can mess around a bit with adding their own data here... should probably make the import more like a file import
+		--Currently only req supported, but others can be made to support if necessary (like power for gens... or Pop for banners)
+		if(_PlayerRace == "chaos_marine_race") then
+		
+			local ChaosMarineRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("chaos_marine_race",false,0,0,0,0,0)	
+			g_FactionSpecificResourceGeneration["chaos_marine_race"] = ChaosMarineRace_PassiveResourceGeneration
+
+		elseif(_PlayerRace == "space_marine_race") then
+		
+			local SpaceMarineRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("space_marine_race", false,0,0,0,0,0)	
+			g_FactionSpecificResourceGeneration["space_marine_race"] = SpaceMarineRace_PassiveResourceGeneration
+			
+		elseif(_PlayerRace == "space_marine_veteran_race") then
+		
+			local SpaceMarineVeteranRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("space_marine_race", false,0,0,0,0,0)	
+			g_FactionSpecificResourceGeneration["space_marine_veteran_race"] = SpaceMarineVeteranRace_PassiveResourceGeneration
+
+		elseif(_PlayerRace == "ork_race") then
+		
+			local OrkRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("ork_race",true,0,0,0.01666666666,0,0)	
+			g_FactionSpecificResourceGeneration["ork_race"] = OrkRace_PassiveResourceGeneration
+
+		elseif(_PlayerRace == "eldar_race") then
+		
+			local EldarRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("eldar_race",false,0,0,0,0,0)	
+			g_FactionSpecificResourceGeneration["eldar_race"] = EldarRace_PassiveResourceGeneration
+			
+		elseif(_PlayerRace == "guard_race") then
+		
+			local GuardRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("guard_race",false,0,0,0,0,0)
+			g_FactionSpecificResourceGeneration["guard_race"] = GuardRace_PassiveResourceGeneration
+			
+		elseif(_PlayerRace == "necron_race") then
+		
+			local NecronRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("necron_race",false,0,0,0,0,0)	
+			NecronRace_PassiveResourceGeneration.BaseMatureLimits.RT_Requisition = 0
+			NecronRace_PassiveResourceGeneration.BaseMatureLimits.RT_Power = 0.66
+			g_FactionSpecificResourceGeneration["necron_race"] = NecronRace_PassiveResourceGeneration
+			
+		elseif(_PlayerRace == "dark_eldar_race") then
+		
+			local DarkEldarRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("dark_eldar_race",true,0,0,0,1,0)
+			g_FactionSpecificResourceGeneration["dark_eldar_race"] = DarkEldarRace_PassiveResourceGeneration
+			
+		elseif(_PlayerRace == "sisters_race") then
+		
+			local SistersRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("sisters_race",false,0,0,0,0,0)	
+			g_FactionSpecificResourceGeneration["sisters_race"] = SistersRace_PassiveResourceGeneration
+
+		elseif(_PlayerRace == "tau_race") then
+		
+			local TauRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("tau_race",false,0,0,0,0,0)	
+			g_FactionSpecificResourceGeneration["tau_race"] = TauRace_PassiveResourceGeneration
+			
+		elseif(_PlayerRace == "inquisition_daemonhunt_race") then
+		
+			local InquisitionDaemonhuntRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("inquisition_daemonhunt_race",false,0,0,0,0,0)	
+			g_FactionSpecificResourceGeneration["inquisition_daemonhunt_race"] = InquisitionDaemonhuntRace_PassiveResourceGeneration
+			
+		end
+
+		table.insert(g_AllRaceResourcesSetup, _PlayerRace)
+
 	end
-
-	import('Upkeep/races/'.._PlayerRace..'.scar')
-	
-	--This code is vulnerable, players can mess around a bit with adding their own data here... should probably make the import more like a file import
-	--Currently only req supported, but others can be made to support if necessary (like power for gens... or Pop for banners)
-	if(_PlayerRace == "chaos_marine_race") then
-	
-		local ChaosMarineRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("chaos_marine_race",false,0,0,0,0,0)	
-		g_FactionSpecificResourceGeneration["chaos_marine_race"] = ChaosMarineRace_PassiveResourceGeneration
-
-	elseif(_PlayerRace == "space_marine_race") then
-	
-		local SpaceMarineRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("space_marine_race", false,0,0,0,0,0)	
-		g_FactionSpecificResourceGeneration["space_marine_race"] = SpaceMarineRace_PassiveResourceGeneration
-		
-	elseif(_PlayerRace == "space_marine_veteran_race") then
-	
-		local SpaceMarineVeteranRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("space_marine_race", false,0,0,0,0,0)	
-		g_FactionSpecificResourceGeneration["space_marine_veteran_race"] = SpaceMarineVeteranRace_PassiveResourceGeneration
-
-	elseif(_PlayerRace == "ork_race") then
-	
-		local OrkRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("ork_race",true,0,0,0.01666666666,0,0)	
-		g_FactionSpecificResourceGeneration["ork_race"] = OrkRace_PassiveResourceGeneration
-
-	elseif(_PlayerRace == "eldar_race") then
-	
-		local EldarRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("eldar_race",false,0,0,0,0,0)	
-		g_FactionSpecificResourceGeneration["eldar_race"] = EldarRace_PassiveResourceGeneration
-		
-	elseif(_PlayerRace == "guard_race") then
-	
-		local GuardRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("guard_race",false,0,0,0,0,0)
-		g_FactionSpecificResourceGeneration["guard_race"] = GuardRace_PassiveResourceGeneration
-		
-	elseif(_PlayerRace == "necron_race") then
-	
-		local NecronRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("necron_race",false,0,0,0,0,0)	
-		NecronRace_PassiveResourceGeneration.BaseMatureLimits.RT_Requisition = 0
-		NecronRace_PassiveResourceGeneration.BaseMatureLimits.RT_Power = 0.66
-		g_FactionSpecificResourceGeneration["necron_race"] = NecronRace_PassiveResourceGeneration
-		
-	elseif(_PlayerRace == "dark_eldar_race") then
-	
-		local DarkEldarRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("dark_eldar_race",true,0,0,0,1,0)
-		g_FactionSpecificResourceGeneration["dark_eldar_race"] = DarkEldarRace_PassiveResourceGeneration
-		
-	elseif(_PlayerRace == "sisters_race") then
-	
-		local SistersRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("sisters_race",false,0,0,0,0,0)	
-		g_FactionSpecificResourceGeneration["sisters_race"] = SistersRace_PassiveResourceGeneration
-
-	elseif(_PlayerRace == "tau_race") then
-	
-		local TauRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("tau_race",false,0,0,0,0,0)	
-		g_FactionSpecificResourceGeneration["tau_race"] = TauRace_PassiveResourceGeneration
-		
-	elseif(_PlayerRace == "inquisition_daemonhunt_race") then
-	
-		local InquisitionDaemonhuntRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("inquisition_daemonhunt_race",false,0,0,0,0,0)	
-		g_FactionSpecificResourceGeneration["inquisition_daemonhunt_race"] = InquisitionDaemonhuntRace_PassiveResourceGeneration
-		
-	end
 	
 
-	table.insert(g_AllRaceResourcesSetup, _PlayerRace)
+	RemoveStacktrace()
 	
 end
 
