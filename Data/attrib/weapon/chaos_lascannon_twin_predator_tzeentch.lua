2f0a59cdc51abba16c1517c2114b4b7f73ae1d30
BenB <TestXBot@gmail.com>
2023-12-02 20:45:52 +0000

    [CSM, SM] Slight buffs to predators v building low and vehicle med

--- Data/attrib/weapon/chaos_lascannon_twin_predator_tzeentch.lua
+++ Data/attrib/weapon/chaos_lascannon_twin_predator_tzeentch.lua
@@ -66,8 +66,8 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"] = Reference([[type_armour\tp_air_med.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"]["screen_name_id"] = [[$4450037]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 1200
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 975
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 1250
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 1000
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 50
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 65
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\chaos_lascannon_hit]]
