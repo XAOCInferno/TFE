cfa2efc59b80e231ccb55dd8af72fb100e61aa3a
BenB <TestXBot@gmail.com>
2023-12-09 12:46:20 +0000

    [DE] Hellion ranged damage small buffs

--- Data/attrib/weapon/dark_eldar_destructor_hellion_leader.lua
+++ Data/attrib/weapon/dark_eldar_destructor_hellion_leader.lua
@@ -65,10 +65,10 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"] = Reference([[type_armour\tp_air_med.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"]["screen_name_id"] = [[$4450037]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 40
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 35
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 42
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 37
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 1
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 5
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 5.5
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Dark_Eldar\splinterpistol_impact]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry02"]["event"] = [[Dark_Eldar\splinterpistol_impact]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry03"]["event"] = [[Dark_Eldar\splinterpistol_impact]]
--- Data/attrib/weapon/dark_eldar_hellglaive_shooter_hellion.lua
+++ Data/attrib/weapon/dark_eldar_hellglaive_shooter_hellion.lua
@@ -65,10 +65,10 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"] = Reference([[type_armour\tp_air_med.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"]["screen_name_id"] = [[$4450037]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 24
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 18
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 28
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 20
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 1
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 1
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 1.1
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\Blood_Splatter_Impact_S]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry02"]["event"] = [[Impact_Bolter_Metal_S]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry03"]["event"] = [[Impact_Bolter_Metal_S]]
