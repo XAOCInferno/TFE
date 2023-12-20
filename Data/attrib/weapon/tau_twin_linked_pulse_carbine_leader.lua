2a838e1facc915e2548948056550f3c6766be181
BenB <TestXBot@gmail.com>
2023-12-20 20:29:50 +0000

    [TAU] Minor drone adjustments

--- Data/attrib/weapon/tau_twin_linked_pulse_carbine_leader.lua
+++ Data/attrib/weapon/tau_twin_linked_pulse_carbine_leader.lua
@@ -64,10 +64,10 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"] = Reference([[type_armour\tp_air_med.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"]["screen_name_id"] = [[$4450037]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 25
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 19
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 4
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 1
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 28
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 20
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 4.5
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 1.2
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Tau\unit_impact\rifle_flesh]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry02"]["event"] = [[Tau\unit_impact\rifle_metal]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry03"]["event"] = [[Tau\unit_impact\rifle_l_metal]]
@@ -134,7 +134,7 @@ GameData["kill_enemy_at_percent_life_target_filter"]["entry_13"]["screen_name_id
 GameData["kill_enemy_at_percent_life_target_filter"]["entry_14"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["kill_enemy_at_percent_life_target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["linger_on_target_after_fire_time"] = 0
-GameData["max_range"] = 13
+GameData["max_range"] = 12.5
 GameData["max_traverse_down"] = -60
 GameData["max_traverse_left"] = -125
 GameData["max_traverse_right"] = 125
