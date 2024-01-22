6d86cd3a9ba74582a4f8373fb9edf56aba446ad6
BenB <TestXBot@gmail.com>
2024-01-22 21:51:47 +0000

    Prototyping - Art\3D - Structures

---
+++ Data/art/ebps/races/tyranids/structures/Hive_nest_big.lua
@@ -0,0 +1,16 @@
+simvis_attributes =
+{
+--	selection_child = 1,
+	selection_ground = 1,
+	
+	vis_scale_min			= 1.9,	
+	vis_scale_max			= 1.9,
+
+	shadow_type = 3,
+
+	occluder = 1,
+        persistent_body = 0 ,
+	distance_game_mesh_lod = 80,
+	distance_nis_mesh_lod = 80,
+
+}
