37b67b6660ada492ea1f401d0d46378dddb09a76
BenB <TestXBot@gmail.com>
2024-01-28 16:53:01 +0000

    [AI] removed ability casters from AI Scaler

--- Data/attrib/ebps/races/world/troops/world_AI_Difficulty_Scaler_Insane.lua
+++ Data/attrib/ebps/races/world/troops/world_AI_Difficulty_Scaler_Insane.lua
@@ -1,6 +1,4 @@
 GameData = Inherit([[]])
-GameData["ability_ext"] = Reference([[ebpextensions\ability_ext.lua]])
-GameData["ability_ext"]["abilities"]["ability_01"] = [[abilities\generic_AI_Scaling_Insane_Ability_Caster.lua]]
 GameData["cost_ext"] = Reference([[ebpextensions\cost_ext.lua]])
 GameData["cost_ext"]["time_cost"]["cost"]["faith"] = 0
 GameData["cost_ext"]["time_cost"]["cost"]["requisition"] = 0
