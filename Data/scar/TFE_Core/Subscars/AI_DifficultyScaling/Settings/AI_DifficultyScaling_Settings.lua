62acbad6a35292ba8631a932ee1e8b8623e631b8
BenB <TestXBot@gmail.com>
2024-01-28 15:22:00 +0000

    [CODE] Finalise AI Difficulty Scaler

--- Data/scar/TFE_Core/Subscars/AI_DifficultyScaling/Settings/AI_DifficultyScaling_Settings.lua
+++ Data/scar/TFE_Core/Subscars/AI_DifficultyScaling/Settings/AI_DifficultyScaling_Settings.lua
@@ -1,9 +1,9 @@
 g_AdvAIDifficultyBonuses = 
 {
-	"world_AI_Difficulty_Scaler_Easy",
-	"world_AI_Difficulty_Scaler_Standard",
-	"world_AI_Difficulty_Scaler_Hard",
-	"world_AI_Difficulty_Scaler_Advanced",
-	"world_AI_Difficulty_Scaler_Insane"
+	{Summon = "world_AI_Difficulty_Scaler_Easy", Modifiers = {{Mod = "production_speed_modifier", Value = 0.8}, {Mod = "cost_time_modifier", Value = 1.2}, {Mod = "cost_requisition_modifier", Value = 1.1}, { Mod = "cost_power_modifier", Value = 1.1 }}},
+	{Summon = "world_AI_Difficulty_Scaler_Standard", Modifiers = {}},
+	{Summon = "world_AI_Difficulty_Scaler_Hard", Modifiers = {{Mod = "production_speed_modifier", Value = 1.1}, {Mod = "cost_time_modifier", Value = 0.9}, {Mod = "cost_requisition_modifier", Value = 0.9}, { Mod = "cost_power_modifier", Value = 0.9 }}},
+	{Summon = "world_AI_Difficulty_Scaler_Advanced", Modifiers = {{Mod = "production_speed_modifier", Value = 1.2}, {Mod = "cost_time_modifier", Value = 0.8}, {Mod = "cost_requisition_modifier", Value = 0.8}, { Mod = "cost_power_modifier", Value = 0.8 }}},
+	{Summon = "world_AI_Difficulty_Scaler_Insane", Modifiers = {{Mod = "production_speed_modifier", Value = 1.4}, {Mod = "cost_time_modifier", Value = 0.6}, {Mod = "cost_requisition_modifier", Value = 0.65}, { Mod = "cost_power_modifier", Value = 0.65 }}},
 }
 	
