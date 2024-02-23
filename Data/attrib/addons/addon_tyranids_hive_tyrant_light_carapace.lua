b9b2194d51880f1b645e27dc4a1ad5b92394d197
XAOCInferno <TestXBot@gmail.com>
2024-02-23 13:28:41 +0000

    Prototyping - Hive Tyrant Addon Crash Fix

--- Data/attrib/addons/addon_tyranids_hive_tyrant_light_carapace.lua
+++ Data/attrib/addons/addon_tyranids_hive_tyrant_light_carapace.lua
@@ -2,6 +2,7 @@ GameData = Inherit([[]])
 GameData["add_on_motion_name"] = [[addon_1]]
 GameData["modifiers"] = Reference([[tables\modifier_table.lua]])
 GameData["modifiers"]["modifier_01"] = Reference([[modifiers\default_weapon_modifier_hardpoint3.lua]])
+GameData["modifiers"]["modifier_01"]["application_type"] = Reference([[type_modifierapplicationtype\tp_mod_apply_to_entity.lua]])
 GameData["modifiers"]["modifier_02"] = Reference([[modifiers\health_maximum_modifier.lua]])
 GameData["modifiers"]["modifier_02"]["shield_of_faith"] = false
 GameData["modifiers"]["modifier_02"]["usage_type"] = Reference([[type_modifierusagetype\tp_mod_usage_addition.lua]])
