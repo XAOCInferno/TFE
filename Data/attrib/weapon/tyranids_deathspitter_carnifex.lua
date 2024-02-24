2fd33f387c8b573d11886ad9a4ceb63ef4e452e0
BenB <TestXBot@gmail.com>
2024-02-24 15:09:53 +0000

    Prototyping - Further work on ranged carnifex, weapons and allowing weapon upgrade

--- Data/attrib/weapon/tyranids_deathspitter_carnifex.lua
+++ Data/attrib/weapon/tyranids_deathspitter_carnifex.lua
@@ -1,5 +1,5 @@
 GameData = Inherit([[]])
-GameData["accuracy"] = 0.55
+GameData["accuracy"] = 0.575
 GameData["accuracy_reduction_when_moving"] = 1
 GameData["area_effect"] = Reference([[tables\area_effect_table.lua]])
 GameData["area_effect"]["area_effect_information"]["remove_modifiers_with_source_entity"] = true 
@@ -67,10 +67,10 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"]["screen_name_id"] = [[$4450037]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["damage_reduction_when_moving"] = 1
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 50 
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 35 
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 65 
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 42 
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 2
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 4
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 4.5
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\Impact_Bolter_Hvy_Blood]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry02"]["event"] = [[unit_impact_events/wraithbone_hit_small]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry03"]["event"] = [[unit_impact_events/Impact_Bolter_Metal_S]]
