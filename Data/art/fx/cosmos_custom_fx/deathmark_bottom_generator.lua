cffc4327f6866e8d1be89f52bcdb3acce40e54e2
BenB <TestXBot@gmail.com>
2024-03-28 21:04:29 +0000

    [NECRON] Fix lights for deathmark and T3 wraith

---
+++ Data/art/fx/cosmos_custom_fx/deathmark_bottom_generator.lua
@@ -0,0 +1,423 @@
+fxtypes =  
+{
+	 
+	{
+		effect = "root",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Fx1",
+					type = "VARTYPE_STRING",
+					value = "rear_engine_glow_idle",
+				},
+				property_01 =  
+				{
+					name = "Fx2",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_02 =  
+				{
+					name = "FX3",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_03 =  
+				{
+					name = "FX4",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_04 =  
+				{
+					name = "FX5",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_05 =  
+				{
+					name = "FX6",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_06 =  
+				{
+					name = "FX7",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_07 =  
+				{
+					name = "FX8",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_08 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_09 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_COMBO",
+		},
+	},
+	 
+	{
+		effect = "root>rear_engine_glow_idle",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Fx1",
+					type = "VARTYPE_STRING",
+					value = "env_barracks_green_light_ring",
+				},
+				property_01 =  
+				{
+					name = "Fx2",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_02 =  
+				{
+					name = "FX3",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_03 =  
+				{
+					name = "FX4",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_04 =  
+				{
+					name = "FX5",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_05 =  
+				{
+					name = "FX6",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_06 =  
+				{
+					name = "FX7",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_07 =  
+				{
+					name = "FX8",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_08 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_09 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_COMBO",
+		},
+	},
+	 
+	{
+		effect = "root>rear_engine_glow_idle>env_barracks_green_light_ring",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Radius",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0.33333,
+						1,
+						0.33333,
+					},
+				},
+				property_01 =  
+				{
+					name = "Offset",
+					type = "VARTYPE_ARRAY_TIMEVECTOR3",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							0,
+							0,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							0,
+							0,
+							0,
+						},
+					},
+				},
+				property_02 =  
+				{
+					name = "SpinX",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_03 =  
+				{
+					name = "SpinY",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_04 =  
+				{
+					name = "SpinZ",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_05 =  
+				{
+					name = "Colour",
+					type = "VARTYPE_ARRAY_TIMECOLOUR",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							38,
+							137,
+							27,
+							255,
+						},
+						entry_01 =  
+						{
+							0.12636,
+							27,
+							101,
+							19,
+							255,
+						},
+						entry_02 =  
+						{
+							0.26688,
+							36,
+							137,
+							25,
+							255,
+						},
+						entry_03 =  
+						{
+							0.43355,
+							27,
+							105,
+							19,
+							255,
+						},
+						entry_04 =  
+						{
+							0.58606,
+							36,
+							137,
+							25,
+							255,
+						},
+						entry_05 =  
+						{
+							0.78976,
+							28,
+							108,
+							19,
+							255,
+						},
+						entry_06 =  
+						{
+							1,
+							36,
+							137,
+							25,
+							255,
+						},
+					},
+				},
+				property_06 =  
+				{
+					name = "RadiusEpsilon",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_07 =  
+				{
+					name = "RateEpsilon",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_08 =  
+				{
+					name = "SpinEpsilonX",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_09 =  
+				{
+					name = "SpinEpsilonY",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_10 =  
+				{
+					name = "SpinEpsilonZ",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+				property_11 =  
+				{
+					name = "SpinRandom",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_12 =  
+				{
+					name = "Duration",
+					type = "VARTYPE_FLOAT",
+					value = 4,
+				},
+				property_13 =  
+				{
+					name = "UseMesh",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_14 =  
+				{
+					name = "UseDepthTest",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_15 =  
+				{
+					name = "UseDepthSort",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_16 =  
+				{
+					name = "Loop",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_17 =  
+				{
+					name = "Billboard",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_18 =  
+				{
+					name = "SelfIlluminated",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_19 =  
+				{
+					name = "Blending",
+					type = "VARTYPE_INT",
+					value = 2,
+				},
+				property_20 =  
+				{
+					name = "Texture",
+					type = "VARTYPE_STRING",
+					value = "TEXTURES_NEW/NECRON/GUN_TUBE_BASE_RING.DDS",
+				},
+				property_21 =  
+				{
+					name = "Mesh",
+					type = "VARTYPE_STRING",
+					value = "Data:art/fx/box.sgm",
+				},
+				property_22 =  
+				{
+					name = "Spawn_Fx",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_23 =  
+				{
+					name = "BirthSpawn_Fx",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_24 =  
+				{
+					name = "SortOrder",
+					type = "VARTYPE_INT",
+					value = 0,
+				},
+				property_25 =  
+				{
+					name = "DepthOffset",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_26 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_27 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_RING",
+		},
+	},
+}
+
---
+++ Data/art/fx/cosmos_custom_fx/generator_orb_deathmarks.lua
@@ -0,0 +1,1115 @@
+fxtypes =  
+{
+	 
+	{
+		effect = "root",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Fx1",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_01 =  
+				{
+					name = "Fx2",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_02 =  
+				{
+					name = "FX3",
+					type = "VARTYPE_STRING",
+					value = "__fxtool_private_copy",
+				},
+				property_03 =  
+				{
+					name = "FX4",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_04 =  
+				{
+					name = "FX5",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_05 =  
+				{
+					name = "FX6",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_06 =  
+				{
+					name = "FX7",
+					type = "VARTYPE_STRING",
+					value = "great_pyramid_doomsday_device_fx1",
+				},
+				property_07 =  
+				{
+					name = "FX8",
+					type = "VARTYPE_STRING",
+					value = "__fxtool_private_copy",
+				},
+				property_08 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_09 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_COMBO",
+		},
+	},
+	 
+	{
+		effect = "root>__fxtool_private_copy",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Radius",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0.35000,
+						0.52250,
+						0.66667,
+						1,
+						0.35000,
+					},
+				},
+				property_01 =  
+				{
+					name = "Offset",
+					type = "VARTYPE_ARRAY_TIMEVECTOR3",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							0,
+							0,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							0,
+							0,
+							0,
+						},
+					},
+				},
+				property_02 =  
+				{
+					name = "SpinX",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_03 =  
+				{
+					name = "SpinY",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_04 =  
+				{
+					name = "SpinZ",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_05 =  
+				{
+					name = "Colour",
+					type = "VARTYPE_ARRAY_TIMECOLOUR",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							187,
+							255,
+							196,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							175,
+							255,
+							185,
+							0,
+						},
+					},
+				},
+				property_06 =  
+				{
+					name = "RadiusEpsilon",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_07 =  
+				{
+					name = "RateEpsilon",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_08 =  
+				{
+					name = "SpinEpsilonX",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_09 =  
+				{
+					name = "SpinEpsilonY",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_10 =  
+				{
+					name = "SpinEpsilonZ",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+				property_11 =  
+				{
+					name = "SpinRandom",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_12 =  
+				{
+					name = "Duration",
+					type = "VARTYPE_FLOAT",
+					value = 2,
+				},
+				property_13 =  
+				{
+					name = "UseMesh",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_14 =  
+				{
+					name = "UseDepthTest",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_15 =  
+				{
+					name = "UseDepthSort",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_16 =  
+				{
+					name = "Loop",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_17 =  
+				{
+					name = "Billboard",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_18 =  
+				{
+					name = "SelfIlluminated",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_19 =  
+				{
+					name = "Blending",
+					type = "VARTYPE_INT",
+					value = 2,
+				},
+				property_20 =  
+				{
+					name = "Texture",
+					type = "VARTYPE_STRING",
+					value = "BIG_MEK_MEGA_MUZZLE_GLOW2.TGA",
+				},
+				property_21 =  
+				{
+					name = "Mesh",
+					type = "VARTYPE_STRING",
+					value = "Data:art/fx/box.sgm",
+				},
+				property_22 =  
+				{
+					name = "Spawn_Fx",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_23 =  
+				{
+					name = "BirthSpawn_Fx",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_24 =  
+				{
+					name = "SortOrder",
+					type = "VARTYPE_INT",
+					value = 1,
+				},
+				property_25 =  
+				{
+					name = "DepthOffset",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_26 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_27 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_RING",
+		},
+	},
+	 
+	{
+		effect = "root>great_pyramid_doomsday_device_fx1",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Fx1",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_01 =  
+				{
+					name = "Fx2",
+					type = "VARTYPE_STRING",
+					value = "spray_explosion",
+				},
+				property_02 =  
+				{
+					name = "FX3",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_03 =  
+				{
+					name = "FX4",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_04 =  
+				{
+					name = "FX5",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_05 =  
+				{
+					name = "FX6",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_06 =  
+				{
+					name = "FX7",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_07 =  
+				{
+					name = "FX8",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_08 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_09 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_COMBO",
+		},
+	},
+	 
+	{
+		effect = "root>great_pyramid_doomsday_device_fx1>spray_explosion",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Emitter_Duration",
+					type = "VARTYPE_FLOAT",
+					value = 2,
+				},
+				property_01 =  
+				{
+					name = "Emitter_Loop",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_02 =  
+				{
+					name = "Emitter_RandomStartTime",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_03 =  
+				{
+					name = "Emitter_ScaleSpeed",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_04 =  
+				{
+					name = "Emitter_Direction",
+					type = "VARTYPE_INT",
+					value = 0,
+				},
+				property_05 =  
+				{
+					name = "Emitter_InheritVelocity",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_06 =  
+				{
+					name = "Emitter_Drag",
+					type = "VARTYPE_FLOAT",
+					value = 0.60000,
+				},
+				property_07 =  
+				{
+					name = "Emitter_Ellipsoidal",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_08 =  
+				{
+					name = "Emitter_Surface",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_09 =  
+				{
+					name = "Emitter_Rate",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						19.13333,
+						1,
+						18.40000,
+					},
+				},
+				property_10 =  
+				{
+					name = "Emitter_Rate_LOD%",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_11 =  
+				{
+					name = "Emitter_Deviation",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						360,
+						1,
+						360,
+					},
+				},
+				property_12 =  
+				{
+					name = "Emitter_Volume",
+					type = "VARTYPE_ARRAY_TIMEVECTOR3",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							0,
+							0,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							0,
+							0,
+							0,
+						},
+					},
+				},
+				property_13 =  
+				{
+					name = "Emitter_Offset",
+					type = "VARTYPE_ARRAY_TIMEVECTOR3",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							0,
+							0,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							0,
+							0,
+							0,
+						},
+					},
+				},
+				property_14 =  
+				{
+					name = "Particle_Speed",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0.10000,
+						1,
+						0.10000,
+					},
+				},
+				property_15 =  
+				{
+					name = "Emitter_RotRate",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_16 =  
+				{
+					name = "Particle_Fx",
+					type = "VARTYPE_STRING",
+					value = "flame_line_beam",
+				},
+				property_17 =  
+				{
+					name = "Particle_Dynamics",
+					type = "VARTYPE_INT",
+					value = 0,
+				},
+				property_18 =  
+				{
+					name = "Particle_Gravity",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+				property_19 =  
+				{
+					name = "Gravwell_Strength",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						9.81000,
+						1,
+						9.81000,
+					},
+				},
+				property_20 =  
+				{
+					name = "Particle_Scale",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0.33333,
+						1,
+						0.70000,
+					},
+				},
+				property_21 =  
+				{
+					name = "Minimum_Cull_Radius",
+					type = "VARTYPE_FLOAT",
+					value = 5,
+				},
+				property_22 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+				property_23 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_SPRAY",
+		},
+	},
+	 
+	{
+		effect = "root>great_pyramid_doomsday_device_fx1>spray_explosion>flame_line_beam",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Duration",
+					type = "VARTYPE_FLOAT",
+					value = 3,
+				},
+				property_01 =  
+				{
+					name = "Loop",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_02 =  
+				{
+					name = "Blending",
+					type = "VARTYPE_INT",
+					value = 2,
+				},
+				property_03 =  
+				{
+					name = "Texture",
+					type = "VARTYPE_STRING",
+					value = "LB3.TGA",
+				},
+				property_04 =  
+				{
+					name = "Offset",
+					type = "VARTYPE_ARRAY_TIMEVECTOR3",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							0,
+							0,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							0,
+							0,
+							0,
+						},
+					},
+				},
+				property_05 =  
+				{
+					name = "Length",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0.13000,
+						1,
+						0.01494,
+					},
+				},
+				property_06 =  
+				{
+					name = "Noise",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_07 =  
+				{
+					name = "Segments",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						1,
+						1,
+						1,
+					},
+				},
+				property_08 =  
+				{
+					name = "Texture_U_Offset",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0.20000,
+						1,
+						0,
+					},
+				},
+				property_09 =  
+				{
+					name = "Texture_U_Repeat",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						1,
+						1,
+						1,
+					},
+				},
+				property_10 =  
+				{
+					name = "Width",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0.40000,
+						0.94750,
+						0.53333,
+						1,
+						1.06667,
+					},
+				},
+				property_11 =  
+				{
+					name = "Colour",
+					type = "VARTYPE_ARRAY_TIMECOLOUR",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							0,
+							50,
+							3,
+							255,
+						},
+						entry_01 =  
+						{
+							0.11765,
+							0,
+							0,
+							0,
+							255,
+						},
+						entry_02 =  
+						{
+							0.19608,
+							60,
+							255,
+							40,
+							255,
+						},
+						entry_03 =  
+						{
+							0.26688,
+							158,
+							255,
+							171,
+							255,
+						},
+						entry_04 =  
+						{
+							0.37732,
+							30,
+							153,
+							0,
+							255,
+						},
+						entry_05 =  
+						{
+							0.80480,
+							0,
+							0,
+							0,
+							255,
+						},
+						entry_06 =  
+						{
+							1,
+							0,
+							0,
+							0,
+							255,
+						},
+					},
+				},
+				property_12 =  
+				{
+					name = "Arc",
+					type = "VARTYPE_ARRAY_TIMEVECTOR3",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							0,
+							0,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							0,
+							0,
+							0,
+						},
+					},
+				},
+				property_13 =  
+				{
+					name = "LengthEpsilon",
+					type = "VARTYPE_FLOAT",
+					value = 0.10000,
+				},
+				property_14 =  
+				{
+					name = "WidthEpsilon",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_15 =  
+				{
+					name = "WorldAlignAxis",
+					type = "VARTYPE_VECTOR3",
+					value =  
+					{
+						0,
+						0,
+						0,
+					},
+				},
+				property_16 =  
+				{
+					name = "UseDepthTest",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_17 =  
+				{
+					name = "SortOrder",
+					type = "VARTYPE_INT",
+					value = 0,
+				},
+				property_18 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+				property_19 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_BEAM",
+		},
+	},
+	 
+	{
+		effect = "root>__fxtool_private_copy",
+		fx =  
+		{
+			properties =  
+			{
+				property_00 =  
+				{
+					name = "Radius",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0.26666,
+						1,
+						0.26666,
+					},
+				},
+				property_01 =  
+				{
+					name = "Offset",
+					type = "VARTYPE_ARRAY_TIMEVECTOR3",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							0,
+							0,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							0,
+							0,
+							0,
+						},
+					},
+				},
+				property_02 =  
+				{
+					name = "SpinX",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_03 =  
+				{
+					name = "SpinY",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_04 =  
+				{
+					name = "SpinZ",
+					type = "VARTYPE_ARRAY_TIMEFLOAT",
+					value =  
+					{
+						0,
+						0,
+						1,
+						0,
+					},
+				},
+				property_05 =  
+				{
+					name = "Colour",
+					type = "VARTYPE_ARRAY_TIMECOLOUR",
+					value =  
+					{
+						entry_00 =  
+						{
+							0,
+							9,
+							255,
+							0,
+							0,
+						},
+						entry_01 =  
+						{
+							1,
+							23,
+							255,
+							0,
+							0,
+						},
+					},
+				},
+				property_06 =  
+				{
+					name = "RadiusEpsilon",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_07 =  
+				{
+					name = "RateEpsilon",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_08 =  
+				{
+					name = "SpinEpsilonX",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_09 =  
+				{
+					name = "SpinEpsilonY",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_10 =  
+				{
+					name = "SpinEpsilonZ",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+				property_11 =  
+				{
+					name = "SpinRandom",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_12 =  
+				{
+					name = "Duration",
+					type = "VARTYPE_FLOAT",
+					value = 2,
+				},
+				property_13 =  
+				{
+					name = "UseMesh",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_14 =  
+				{
+					name = "UseDepthTest",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_15 =  
+				{
+					name = "UseDepthSort",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_16 =  
+				{
+					name = "Loop",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_17 =  
+				{
+					name = "Billboard",
+					type = "VARTYPE_BOOL",
+					value = 1,
+				},
+				property_18 =  
+				{
+					name = "SelfIlluminated",
+					type = "VARTYPE_BOOL",
+					value = 0,
+				},
+				property_19 =  
+				{
+					name = "Blending",
+					type = "VARTYPE_INT",
+					value = 2,
+				},
+				property_20 =  
+				{
+					name = "Texture",
+					type = "VARTYPE_STRING",
+					value = "TEXTURES_NEW/NECRON/GUN_TUBE_BASE_RING.DDS",
+				},
+				property_21 =  
+				{
+					name = "Mesh",
+					type = "VARTYPE_STRING",
+					value = "Data:art/fx/box.sgm",
+				},
+				property_22 =  
+				{
+					name = "Spawn_Fx",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_23 =  
+				{
+					name = "BirthSpawn_Fx",
+					type = "VARTYPE_STRING",
+					value = "",
+				},
+				property_24 =  
+				{
+					name = "SortOrder",
+					type = "VARTYPE_INT",
+					value = 1,
+				},
+				property_25 =  
+				{
+					name = "DepthOffset",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_26 =  
+				{
+					name = "Detail_From",
+					type = "VARTYPE_FLOAT",
+					value = 0,
+				},
+				property_27 =  
+				{
+					name = "Detail_To",
+					type = "VARTYPE_FLOAT",
+					value = 1,
+				},
+			},
+			style = "STYLE_RING",
+		},
+	},
+}
+
