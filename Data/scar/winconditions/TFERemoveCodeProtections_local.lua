7ff8d0164f69c228f64e7e4d96c57d95c116abeb
BenB <TestXBot@gmail.com>
2024-04-16 17:01:45 +0100

    [CODE] Added debug feature to disable pcall protection

---
+++ Data/scar/winconditions/TFERemoveCodeProtections_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = false,
+always_on = false,
+title = "TFE Remove Code Protections",
+win_message = "",
+lose_message = "",
+description = "Removes all protections on code, allowing for 'Scar Errors'. Do not include in shipped mod."
+}
