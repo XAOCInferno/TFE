8f9dca3b6268b5bbddac4afbb62548f7487ed805
BenB <TestXBot@gmail.com>
2024-05-13 23:34:46 +0100

    [CODE] Final additions to weapons exporter

--- Designs/WeaponPiercings/ListAllLuaFromDirectory.py
+++ Designs/WeaponPiercings/ListAllLuaFromDirectory.py
@@ -4,7 +4,7 @@ def GetFileNames(path, extension, doPrint):
     acceptedFiles = []
     files = []
     
-    for path, subdirs, files in os.walk(path):
+    for path, _, files in os.walk(path):
         for name in files:
             files.clear()
             files = os.listdir(path)
@@ -15,6 +15,7 @@ def GetFileNames(path, extension, doPrint):
               if split[1] == "." + extension:
                   acceptedFiles.append(split[0])
 
+    #Debug print
     if doPrint:   
         for acceptedFile in acceptedFiles: 
             print('"' + acceptedFile + '",')
