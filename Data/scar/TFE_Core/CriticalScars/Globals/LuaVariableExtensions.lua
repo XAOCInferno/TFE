ba51375b7bdc87d90cc62bc9809c87993e8a7bd9
BenB <TestXBot@gmail.com>
2024-05-19 22:23:42 +0100

    [CODE] Slight edit to the Lua extensions. remove unnecessary lua file

--- Data/scar/TFE_Core/CriticalScars/Globals/LuaVariableExtensions.lua
+++
@@ -1,9 +0,0 @@
-debug_VariableExtensions_FileName = "LuaVariableExtensions"
-
-
-function PlusEquals (_Variable, _AddTo)
-
-	--Simple Adder function don't need StackTrace
-	return _Variable + _AddTo
-
-end
