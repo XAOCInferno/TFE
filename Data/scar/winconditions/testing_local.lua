5c022476db93b682302e2cbfca0624bf3bc07c2e
BenB <TestXBot@gmail.com>
2024-06-16 12:16:59 +0100

    [CODE] Move Sandbox mode scars into mod folder

---
+++ Data/scar/winconditions/testing_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = false,
+always_on = false,
+title = "Testing Mode",
+win_message = "",
+lose_message = "",
+description = "Your resources are always 5000/5000. Instant production and building"
+}
