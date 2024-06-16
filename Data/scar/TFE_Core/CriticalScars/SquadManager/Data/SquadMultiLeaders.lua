a0d81c99c63d68bb033971bd4b3a41a168cd3106
BenB <TestXBot@gmail.com>
2024-06-16 16:26:37 +0100

    [CODE] Time travel extra functionality, leaders and loadouts

---
+++ Data/scar/TFE_Core/CriticalScars/SquadManager/Data/SquadMultiLeaders.lua
@@ -0,0 +1,40 @@
+g_MultiSquadLeaders = 
+{
+	chaos_marine_race = 
+	{
+		chaos_AL_Operative_squad = { "chaos_al_psyker_leader", "chaos_AL_Operative_leader_grenade", "chaos_AL_Operative_leader_flamer", "chaos_AL_Operative_leader_melta" }
+	},
+	
+	dark_eldar_race = 
+	{
+		dark_eldar_squad_archon = { "dark_eldar_leader_incubus", "dark_eldar_leader_mandrake", "dark_eldar_leader_wych", "dark_eldar_leader_dog" }
+	},
+	
+	eldar_race = {},
+	
+	guard_race = 
+	{
+		guard_squad_command_squad = { "guard_leaders_psyker_command_squad", "guard_leaders_priest_command_squad", "guard_leaders_commissar_command_squad" }
+	},
+	
+	inquisition_daemonhunt_race = 
+	{
+		inquisition_squad_bodyguard = { "inquisition_inquisitor_acolyte", "inquisition_scholars_exorcist" },
+		inquisition_squad_inquisitor_lord_cs = { "inquisition_servo_skull", "inquisition_acolyte_cs", "inquisition_exorcist_cs", "inquisition_crusader_cs" },
+		inquisition_squad_scholars = { "inquisition_scholars_calculus_logi", "inquisition_scholars_astropath" }
+	},
+	
+	necron_race = {},	
+	ork_race = {},	
+	
+	sisters_race = 
+	{
+		sisters_squad_celestian = { "sisters_veteran_superior_celestian", "sisters_celestian_imagifer" }
+	},
+	
+	space_marine_race = {},		
+	space_marine_veteran_race = {},
+	tau_race = {},		
+	world_race = {}
+
+}
---
+++ Data/scar/TFE_Core/CriticalScars/SquadManager/Data/SquadValidWeapons.lua
@@ -0,0 +1,111 @@
+g_ValidSquadWeapons = 
+{
+	chaos_marine_race = 
+	{
+	
+		chaos_squad_cultist = { "chaos_plasma_gun_cultist", "chaos_grenade_launcher" },
+		chaos_marine_squad = { "chaos_heavy_bolter_tactical", "chaos_plasma_gun_tactical" },
+		chaos_squad_noise_marine = { "chaos_noise_blaster" },
+		chaos_squad_predator = { "chaos_lascannon_twin_predator", "chaos_lascannon_predator_r", "chaos_lascannon_predator_l" },
+		chaos_squad_raptor = { "chaos_flamer_raptor" }
+		
+	},
+	
+	dark_eldar_race = 
+	{
+	
+		dark_eldar_squad_ravager = { "dark_eldar_disintegrator_ravager", "dark_eldar_disintegrator_ravager_2", "dark_eldar_disintegrator_ravager_3" }
+		
+	},
+	
+	eldar_race = 
+	{
+		eldar_squad_falcon_grav_tank = { "eldar_starcannon_grav_tank" },
+		eldar_squad_wraithlord = { "eldar_brightlance_wraithlord" }
+	},
+	
+	guard_race = 
+	{
+	
+		guard_heavy_weapons_team_squad = { "guard_lasgun_heavy_weapons_team", "guard_autocannon_heavy_weapons_team" },
+		guard_squad_guardsmen = { "guard_grenade_launcher_guardsmen", "guard_plasma_gun_guardsmen" },
+		guard_squad_kasrkin = { "guard_grenade_launcher_kasrkin", "guard_plasma_gun_kasrkin" },
+		guard_squad_sentinel = { "guard_heavy_flamer_sentinel", "guard_multi_laser_sentinel" },
+		guard_squad_lemanruss = { "guard_lascannon_leman_russ_right", "guard_lascannon_leman_russ_left" }
+		
+	},
+	
+	inquisition_daemonhunt_race = 
+	{
+	
+		inquisition_squad_bodyguard = {"inquisition_flamer_bodyguard", "inquisition_plasma_gun_bodyguard", "inquisition_melta_gun_bodyguard" },
+		inquisition_squad_dreadnought_assault = { "inquisition_assault_cannon_dreadnought", "inquisition_multi_melta_gun_dreadnought" },
+		inquisition_squad_dreadnought_support = { "inquisition_autocannon_twin_dreadnought", "inquisition_heavy_plasma_gun_dreadnought", "inquisition_lascannon_twin_dreadnought" },
+		inquisition_squad_exorcist_sister = { "inquisition_incinerator_exorcist" },
+		inquisition_squad_grey_knights_interceptors = { "inquisition_nemesis_halberd_grey_knight", "inquisition_thunder_hammer_grey_knight" },
+		inquisition_squad_grey_knights_paladin = { "inquisition_incinerator_paladin", "inquisition_psycannon_paladin" },
+		inquisition_squad_grey_knights_purgation = { "inquisition_psycannon_grey_knight" },
+		inquisition_squad_grey_knights_terminator = { "inquisition_incinerator_gk_terminator", "inquisition_psycannon_gk_terminator" },
+		inquisition_squad_razorback = { "inquisition_lascannon_razorback" },
+		inquisition_squad_servitor_ranged = { "inquisition_heavy_bolter_servitor" },
+		inquisition_squad_shock_troops = { "inquisition_grenade_launcher_incendiary_shock_troops", "inquisition_grenade_launcher_shock_troops", "inquisition_plasma_gun_shock_troops", "inquisition_melta_gun_shock_troops" },
+		inquisition_squad_shock_troops_extra = { "inquisition_grenade_launcher_incendiary_shock_troops", "inquisition_grenade_launcher_shock_troops", "inquisition_plasma_gun_shock_troops", "inquisition_melta_gun_shock_troops" },
+		inquisition_squad_techmarine = { "inquisition_flamer_combo", "inquisition_plasma_combi_bolter", "inquisition_melta_combo" }	
+		
+	},
+	
+	necron_race = 
+	{
+	
+		necron_tomb_spyder_squad = { "necron_tomb_spyder_claw_splash" }	
+		
+	},
+	
+	ork_race = 
+	{
+	
+		ork_squad_killa_kan = { "ork_rokkit_launcha_killakan", "ork_rokkit_launcha_killakan_free" },
+		ork_squad_shoota_boy = { "ork_big_shoota_shoota", "ork_big_shoota_shoota_free" },
+		ork_squad_wartrak = { "ork_bomb_chucka_wartrack", "ork_bomb_chucka_wartrack_free" },
+		ork_squad_nob = { "ork_power_claw_nobz" },
+		ork_squad_slugga = { "ork_burna_slugga", "ork_burna_slugga_free" }
+		
+	},		
+	
+	sisters_race = 
+	{
+		
+		sisters_squad_battle_sister = { "sisters_flamer_battle_sister", "sisters_heavy_bolter_battle_sister" },
+		sisters_squad_celestian = { "sisters_melta_celestian", "sisters_multi_melta_celestian" },
+		sisters_squad_immolator_tank = { "sisters_multi_melta_immolator" }
+		
+	},
+	
+	space_marine_race = 
+	{
+	
+		space_marine_squad_devastator = { "space_marine_lascannon_devastator", "space_marine_plasma_cannon_devastator" },
+		space_marine_squad_dreadnought = { "space_marine_assault_cannon_dreadnought", "space_marine_lascannon_twin_dreadnought" },
+		space_marine_squad_predator = { "space_marine_lascannon_twin_predator", "space_marine_lascannon_tank_right", "space_marine_lascannon_tank_left" },
+		space_marine_squad_scout = { "space_marine_flamer_scout", "space_marine_sniper_rifle", "space_marine_plasma_gun_scout" },
+		space_marine_squad_tactical = { "space_marine_flamer_tactical", "space_marine_heavy_bolter_tactical", "space_marine_plasma_gun" },
+		space_marine_squad_tempest = { "space_marine_missile_frag_tempest", "space_marine_missile_krak_tempest" },
+		space_marine_squad_terminator = { "space_marine_heavy_flamer", "space_marine_assault_cannon_terminator" },
+		space_marine_squad_terminator_fc = { "space_marine_heavy_flamer_fc", "space_marine_assault_cannon_terminator_fc" }	
+		
+	},
+	
+	space_marine_veteran_race = {},	
+	
+	tau_race = 
+	{
+	
+		tau_commander_squad = { "tau_fusion_blaster_commander", "tau_plasma_rifle_commander", "tau_missile_launcher_commander", "tau_flamer_commander" },
+		tau_stealth_team_squad = { "tau_fusion_blaster_stealth_suit" },
+		tau_crisis_suit_squad = { "tau_missile_pod_crisis_suit", "tau_flamer_crisis_suit" }
+		
+	},
+		
+	world_race = {}
+
+}
