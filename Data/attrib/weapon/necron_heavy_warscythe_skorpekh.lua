e61299570de616d76a08575eb3d44c3dc842b43b
BenB <TestXBot@gmail.com>
2024-07-07 15:12:34 +0100

    [NECRON] Skorpekh small nerfs

--- Data/attrib/weapon/necron_heavy_warscythe_skorpekh.lua
+++ Data/attrib/weapon/necron_heavy_warscythe_skorpekh.lua
@@ -79,8 +79,8 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_18"]["armour_type"] = Reference([[type_armour\tp_sub_commander.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_18"]["armour_type"]["screen_name_id"] = [[$90103]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 250
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 200
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 240
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 185
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 20
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 20
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry05"] = Reference([[tables\hit_event_entry.lua]])
--- Data/attrib/weapon/necron_warscythe_skorpekh.lua
+++ Data/attrib/weapon/necron_warscythe_skorpekh.lua
@@ -79,9 +79,9 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_18"]["armour_type"] = Reference([[type_armour\tp_sub_commander.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_18"]["armour_type"]["screen_name_id"] = [[$90103]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 200
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 160
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 1
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 165
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 135
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 10
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 14
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry05"] = Reference([[tables\hit_event_entry.lua]])
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry05"]["surface"] = Reference([[type_surface\tp_monolith.lua]])
