6ae94e2197688cd1351ff47280c9a5ef43fb035d
BenB <TestXBot@gmail.com>
2024-07-17 00:12:40 +0100

    [DE] Better passive souls generation

--- Data/scar/TFE_Core/Subscars/Resources/AutoGenResources/PassiveResourceGeneration_Settings.lua
+++ Data/scar/TFE_Core/Subscars/Resources/AutoGenResources/PassiveResourceGeneration_Settings.lua
@@ -50,7 +50,7 @@ function Setup_PassiveResourceGeneration_Settings(_PlayerRace)
 			
 		elseif(_PlayerRace == "dark_eldar_race") then
 		
-			local DarkEldarRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("dark_eldar_race",true,ResourceGroup:new(0,0,0,0,1.25))
+			local DarkEldarRace_PassiveResourceGeneration = SpecificRaceResourceGenerationDetails:new("dark_eldar_race",true,ResourceGroup:new(0,0,0,0,2))
 			g_FactionSpecificResourceGeneration["dark_eldar_race"] = DarkEldarRace_PassiveResourceGeneration
 			
 		elseif(_PlayerRace == "sisters_race") then
