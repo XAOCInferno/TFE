a5638d63b038146124aa973412b6a85f1aec1880
BenB <TestXBot@gmail.com>
2024-08-29 20:47:53 +0100

    [ELDAR] Rework ranger snare mine so that it cannot trigger multiple times

---
+++ Data/art/ebps/races/eldar/projectiles/plasma_snare_mine.lua
@@ -0,0 +1,17 @@
+simvis_attributes =
+{
+
+	vis_scale_min			= 5,
+	vis_scale_max			= 5,
+
+	-- Shadow Types:
+	-- 0 = None
+	-- 1 = blob (rotated by light dir)
+	-- 2 = blob (rotated by object dir)
+	-- 3 = shadow map
+	-- 4 = stencil shadow volume
+	shadow_type			= 1,
+	shadow_scale			= 0.019,
+	
+		occludee	= 1,
+}
