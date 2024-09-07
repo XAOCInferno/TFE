e931ff19ee59114f4e1a9f84fe09b09f54282942
omar\Col <omarsarakbi206@gmail.com>
2024-09-07 19:04:40 +0300

    [OM&SISTERS] small balance adjustments

--- Data/attrib/weapon/inquisition_hellgun_bodyguard.lua
+++ Data/attrib/weapon/inquisition_hellgun_bodyguard.lua
@@ -82,8 +82,8 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_19"]["armour_type"] = Reference([[type_armour\tp_sub_commander.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_19"]["armour_type"]["screen_name_id"] = [[$90103]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 18.75
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 13.5
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 16.75
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 12.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 1
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 3
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\laspistol_hit]]
