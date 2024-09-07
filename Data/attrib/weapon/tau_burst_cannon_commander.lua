6dc2f4140ff551cdeb79c20c07bccaafd53dc168
BenB <TestXBot@gmail.com>
2024-09-07 01:01:58 +0100

    [TAU] Nerf TC burst cannon

--- Data/attrib/weapon/tau_burst_cannon_commander.lua
+++ Data/attrib/weapon/tau_burst_cannon_commander.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\ranged_burst_cannon_light.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 65
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 45
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 30
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 18
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 2
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 10
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Tau\unit_impact\burst_cannon_flesh]]
