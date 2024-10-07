95feea81f3d61ec09b10ff27353b0918bd38d3ff
BenB <TestXBot@gmail.com>
2024-10-07 19:47:59 +0100

    [GUARD] Added reload system to missing units and removed stonewall

--- Data/attrib/ebps/races/guard/troops/guard_infantry_guardsmen.lua
+++ Data/attrib/ebps/races/guard/troops/guard_infantry_guardsmen.lua
@@ -2,7 +2,6 @@ GameData = Inherit([[]])
 GameData["ability_ext"] = Reference([[ebpextensions\ability_ext.lua]])
 GameData["ability_ext"]["abilities"]["ability_01"] = [[abilities\guard_full_retreat.lua]]
 GameData["ability_ext"]["abilities"]["ability_11"] = [[abilities\generic_auto_force_reload.lua]]
-GameData["ability_ext"]["abilities"]["ability_12"] = [[abilities\generic_stonewall_type_ranged.lua]]
 GameData["combat_ext"] = Reference([[ebpextensions\combat_ext.lua]])
 GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["attack_motion_variable_name"] = [[Weapon_Range_Tracking_Main]]
 GameData["combat_ext"]["hardpoints"]["hardpoint_01"]["hardpoint_weapon_variant_motion_name"] = [[Weapon_Range_Upgrade_Main]]
