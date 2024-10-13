49c23f2eb900d40ddbb896563477061207402736
omar\Col <omarsarakbi206@gmail.com>
2024-10-13 18:00:48 +0300

    [OM] Polish and Balance patch part 2

--- Data/attrib/weapon/inquisition_storm_bolter_bro_cap.lua
+++ Data/attrib/weapon/inquisition_storm_bolter_bro_cap.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Storm_Bolt.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 110
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 80
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 114
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 82.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 2.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 7
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events/Blood_Splatter_Impact_S]]
