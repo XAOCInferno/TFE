a57b15dca84bcd3a9105c2e3f806d898e29eb5e1
BenB <TestXBot@gmail.com>
2024-10-13 14:33:35 +0100

    [DE] Idle soul gathering for slaves

---
+++ Data/scar/TFE_Core/CriticalScars/SquadManager/Data/SquadIdleBehaviours.lua
@@ -0,0 +1,7 @@
+eIDLE_STATE_BEHAVIOUR_NONE = 0
+eIDLE_STATE_BEHAVIOUR_CANNIBALIZE = 1
+
+g_SquadIdleBehaviours = 
+{
+	dark_eldar_squad_slave = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 15 }
+}
