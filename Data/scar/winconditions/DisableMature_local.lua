221203e4946633f86fc39b3343d699012fe5de3d
BenB <TestXBot@gmail.com>
2024-10-20 16:16:21 +0100

    [CODE] Toggles for disabling upkeep and mature

---
+++ Data/scar/winconditions/DisableMature_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = false,
+always_on = false,
+title = "TFE Disable Mature",
+win_message = "",
+lose_message = "",
+description = "Turns off mature for listening posts."
+}
---
+++ Data/scar/winconditions/DisableUpkeep_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+exclusive = false,
+victory_condition = false,
+always_on = false,
+title = "TFE Disable Upkeep",
+win_message = "",
+lose_message = "",
+description = "Turns off upkeep."
+}
