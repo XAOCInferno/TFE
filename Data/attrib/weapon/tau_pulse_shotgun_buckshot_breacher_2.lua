bf8c2e8b70956fe51cdc7cd4242baeceefedd065
BenB <TestXBot@gmail.com>
2024-10-26 21:33:28 +0100

    [TAU] AoE scaling on breacher per child and small damage increase

--- Data/attrib/weapon/tau_pulse_shotgun_buckshot_breacher_2.lua
+++ Data/attrib/weapon/tau_pulse_shotgun_buckshot_breacher_2.lua
@@ -1,5 +1,6 @@
 GameData = Inherit([[weapon\tau_pulse_shotgun_buckshot_breacher_1.lua]])
 GameData["accuracy"] = GameData["accuracy"] + GameData["accuracy_change_per_children"]
 GameData["accuracy_reduction_when_moving"] = GameData["accuracy"] * (1 - GameData["accuracy_when_moving_as_percent_of_accuracy"])
+GameData["area_effect"]["area_effect_information"]["radius_change_for_child_weapons"] = GameData["area_effect"]["area_effect_information"]["radius_change_for_child_weapons"] + GameData["area_effect"]["area_effect_information"]["radius_change_for_child_weapons"]
 GameData["max_range"] = GameData["max_range"] + GameData["max_range_change_for_child_weapons"]
 GameData["shot_delay_time"] = GameData["shot_delay_time"] + GameData["shot_delay_time_change_for_child_weapons"]
