17ca9e7a4ab9b85033372c5ecfb367567a7df74d
omar\Col <omarsarakbi206@gmail.com>
2024-11-01 00:34:28 +0200

    [SM] Force commander melee damage nerf and fix

--- Data/attrib/weapon/space_marine_daemon_hammer.lua
+++ Data/attrib/weapon/space_marine_daemon_hammer.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\melee_heavy.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 420
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 360
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 355
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 290
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 25
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 75
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry05"] = Reference([[tables\hit_event_entry.lua]])
