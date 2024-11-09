7ee6f4c307c85ccca85e37eea2e0b5ce05b32fbc
BenB <TestXBot@gmail.com>
2024-11-09 20:55:04 +0000

    [ELDAR] Repear launcher further nerfs and piercing change

--- Data/attrib/weapon/eldar_reaper_launcher_base.lua
+++ Data/attrib/weapon/eldar_reaper_launcher_base.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Reaper_Launcher.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 20
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 14
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 18
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 13
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 1
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 4.5
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\reaper_hit]]
