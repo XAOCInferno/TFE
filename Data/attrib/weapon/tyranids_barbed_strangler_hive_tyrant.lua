2a11af7099bd893645aa04a50ad91f618a1566cf
BenB <TestXBot@gmail.com>
2024-11-23 13:45:06 +0000

    [NIDS] Further buff hive tyrant barbed strangler

--- Data/attrib/weapon/tyranids_barbed_strangler_hive_tyrant.lua
+++ Data/attrib/weapon/tyranids_barbed_strangler_hive_tyrant.lua
@@ -8,7 +8,7 @@ GameData["area_effect"]["area_effect_information"]["area_effect_events"]["rock"]
 GameData["area_effect"]["area_effect_information"]["area_effect_events"]["water"] = [[unit_impact_events/barbed_strangler_explosion]]
 GameData["area_effect"]["area_effect_information"]["area_type"] = Reference([[TYPE_AreaEffect\tp_area_effect_circle.lua]])
 GameData["area_effect"]["area_effect_information"]["filter_type"] = Reference([[TYPE_AreaFilter\tp_area_filter_enemy.lua]])
-GameData["area_effect"]["area_effect_information"]["radius"] = 5.5
+GameData["area_effect"]["area_effect_information"]["radius"] = 6
 GameData["area_effect"]["area_effect_information"]["remove_modifiers_with_source_entity"] = true 
 GameData["area_effect"]["area_effect_information"]["start_from_caster"] = false
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing"] = 2
@@ -58,10 +58,10 @@ GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"] = Reference([[type_armour\tp_air_med.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"]["entry_15"]["armour_type"]["screen_name_id"] = [[$4450037]]
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 145
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 110
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 10
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 30
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 240
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 190
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 20
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 50
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\barbed_strangler_hit]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry02"]["event"] = [[Unit_Impact_events\barbed_strangler_hit]]
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry03"]["event"] = [[Unit_Impact_events\barbed_strangler_hit]]
