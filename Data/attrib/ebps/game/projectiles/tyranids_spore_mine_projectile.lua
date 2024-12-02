48add084560f9c6e2ffed17a9ed4271a08c76dc3
BenB <TestXBot@gmail.com>
2024-12-02 20:34:28 +0000

    [NIDS] attempted fix for biovore spore mine

--- Data/attrib/ebps/game/projectiles/tyranids_spore_mine_projectile.lua
+++ Data/attrib/ebps/game/projectiles/tyranids_spore_mine_projectile.lua
@@ -7,10 +7,11 @@ GameData["entity_blueprint_ext"]["scale_z"] = 0.25
 GameData["projectile_ext"] = Reference([[ebpextensions\projectile_ext.lua]])
 GameData["projectile_ext"]["explode_on_miss"] = true 
 GameData["projectile_ext"]["artillery"]["is_artillery"] = true
-GameData["projectile_ext"]["artillery"]["offtarget_radius_blindfire"] = 6
-GameData["projectile_ext"]["lifetime_as_percent_of_max_range_time"] = 105
+GameData["projectile_ext"]["artillery"]["offtarget_radius_blindfire"] = 8
+GameData["projectile_ext"]["lifetime_as_percent_of_max_range_time"] = 120
 GameData["projectile_ext"]["really_able_attack_ground_no_seriously_for_real"] = true 
-GameData["projectile_ext"]["speed"] = 60
+GameData["projectile_ext"]["speed"] = 55
 GameData["sim_entity_ext"] = Reference([[ebpextensions\sim_entity_ext.lua]])
 GameData["sim_entity_ext"]["is_in_spatial_bucket"] = false
 GameData["type_ext"] = Reference([[ebpextensions\type_ext.lua]])
+
