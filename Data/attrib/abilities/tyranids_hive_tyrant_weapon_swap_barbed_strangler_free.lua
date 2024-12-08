873c5b43ad44d1eb36467028f89d0d46a7eb4d0e
BenB <TestXBot@gmail.com>
2024-12-08 14:45:45 +0000

    Reapply "[NIDS] Pre-setup for Hive Tyrant free weapon swap system"
    This reverts commit ecab412a0ea991d4c5d3da088a1e00b7b0be5e19.

---
+++ Data/attrib/abilities/tyranids_hive_tyrant_weapon_swap_barbed_strangler_free.lua
@@ -0,0 +1,9 @@
+GameData = Inherit([[abilities\tyranids_hive_tyrant_weapon_swap_barbed_strangler.lua]])
+GameData["fire_cost"] = Reference([[tables\cost_table.lua]])
+GameData["fire_cost"]["faith"] = 0
+GameData["fire_cost"]["population"] = 0
+GameData["fire_cost"]["power"] = 0
+GameData["fire_cost"]["requisition"] = 0
+GameData["fire_cost"]["souls"] = 0
+GameData["fire_cost"]["time_seconds"] = 0
+GameData["recharge_time"] = 10
