94d14e295958e8103c5007b0db2d7682f7b83e5a
BenB <TestXBot@gmail.com>
2024-12-20 00:13:05 +0000

    [SISTERS] Decrease faith production of pray ability

--- Data/attrib/ebps/environment/gameplay/sisters_pray_entity.lua
+++ Data/attrib/ebps/environment/gameplay/sisters_pray_entity.lua
@@ -1,6 +1,6 @@
 GameData = Inherit([[]])
 GameData["resource_ext"] = Reference([[ebpextensions\resource_ext.lua]])
-GameData["resource_ext"]["faith_per_second"] = 0.1000000015
+GameData["resource_ext"]["faith_per_second"] = 0.05
 GameData["sim_entity_ext"] = Reference([[ebpextensions\sim_entity_ext.lua]])
 GameData["suicide_ext"] = Reference([[ebpextensions\suicide_ext.lua]])
 GameData["suicide_ext"]["lifetime"] = 1
