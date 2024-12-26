b1ae7b110d6bf93df69a26a5356a058d51be0d62
BenB <TestXBot@gmail.com>
2024-12-26 23:12:03 +0000

    [GUARD] Execute now gives more mass

--- Data/attrib/abilities/guard_commissar_execute.lua
+++ Data/attrib/abilities/guard_commissar_execute.lua
@@ -45,7 +45,7 @@ GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_06"]["modifier"]
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_07"]["modifier"] = Reference([[modifiers\special_attack_physics_mass.lua]])
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_07"]["modifier"]["shield_of_faith"] = false
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_07"]["modifier"]["usage_type"] = Reference([[type_modifierusagetype\tp_mod_usage_addition.lua]])
-GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_07"]["modifier"]["value"] = 6 --500
+GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_07"]["modifier"]["value"] = 10 --500
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_08"] = Reference([[tables\time_modify_entry.lua]])
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_08"]["modifier"]["shield_of_faith"] = false
 GameData["area_effect"]["weapon_damage"]["modifiers"]["modifier_09"] = Reference([[tables\time_modify_entry.lua]])
