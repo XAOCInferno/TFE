040c63dced9a5c063342692737edd1d73462671d
BenB <TestXBot@gmail.com>
2024-12-29 14:38:05 +0000

    [CHAOS] Worship now provides some small combat bonuses

--- Data/attrib/ebps/environment/gameplay/chaos_worship_aura_entity.lua
+++ Data/attrib/ebps/environment/gameplay/chaos_worship_aura_entity.lua
@@ -3,6 +3,7 @@ GameData["ability_ext"] = Reference([[ebpextensions\ability_ext.lua]])
 GameData["ability_ext"]["abilities"]["ability_01"] = [[abilities\chaos_taint_ability.lua]]
 GameData["ability_ext"]["abilities"]["ability_02"] = [[abilities\chaos_taint_ability_heal.lua]]
 GameData["ability_ext"]["abilities"]["ability_03"] = [[abilities\chaos_taint_comm_heal.lua]]
+GameData["ability_ext"]["abilities"]["ability_04"] = [[abilities\chaos_worship_bonuses.lua]]
 GameData["ability_ext"]["abilities"]["ability_12"] = [[abilities\chaos_taint_ability_t1.lua]]
 GameData["entity_blueprint_ext"] = Reference([[ebpextensions\entity_blueprint_ext.lua]])
 GameData["entity_blueprint_ext"]["animator"] = [[Environment\Necron\solar_pulse]]
