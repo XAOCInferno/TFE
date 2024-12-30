a234083e34b3fa90bee275a049ed510359c8a7e3
BenB <TestXBot@gmail.com>
2024-12-30 12:47:34 +0000

    [CRON, TAU] FO and Kroots will now cannibalize if left idle for a time

--- Data/scar/TFE_Core/CriticalScars/SquadManager/Data/SquadIdleBehaviours.lua
+++ Data/scar/TFE_Core/CriticalScars/SquadManager/Data/SquadIdleBehaviours.lua
@@ -6,5 +6,8 @@ g_SquadIdleBehaviours =
 {
 	dark_eldar_squad_slave = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 15 },
 	dark_eldar_squad_mandrake = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 35 },
+	tau_kroot_carnivore_squad = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 15 },
+	necron_flayed_one_squad = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 15 },
+	
 	chaos_squad_cultist = { Behaviour = eIDLE_STATE_BEHAVIOUR_WORSHIP_CHAOS, TimeMustBeIdleFor = 2, AbilityCaster = "" }
 }
