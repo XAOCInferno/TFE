0592b43f2fdea4736af2ba96fb4181fdc81cf0a4
BenB <TestXBot@gmail.com>
2024-12-30 16:32:29 +0000

    [CODE] Setup idle behaviours for squads in data

--- Data/scar/TFE_Core/CriticalScars/SquadManager/Data/SquadIdleBehaviours.lua
+++ Data/scar/TFE_Core/CriticalScars/SquadManager/Data/SquadIdleBehaviours.lua
@@ -1,13 +1,34 @@
 eIDLE_STATE_BEHAVIOUR_NONE = 0
 eIDLE_STATE_BEHAVIOUR_CANNIBALIZE = 1
 eIDLE_STATE_BEHAVIOUR_WORSHIP_CHAOS = 2
+eIDLE_STATE_BEHAVIOUR_REPAIR = 3
+eIDLE_STATE_BEHAVIOUR_CONSTRUCT = 4
+eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT = 5
+eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT_CANNIBALIZE = 6
 
 g_SquadIdleBehaviours = 
 {
-	dark_eldar_squad_slave = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 15 },
+	--[[Builders]]
+	dark_eldar_squad_slave = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT_CANNIBALIZE, TimeMustBeIdleFor = 15, Radius = 40 },
+	chaos_squad_slave = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	eldar_squad_bonesinger = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	eldar_squad_bonesinger_start = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	guard_squad_enginseer = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	inquisition_squad_archivist = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	necron_builder_scarab_squad = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	ork_squad_grot = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	sisters_squad_servitor = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	space_marine_squad_servitor = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	space_marine_squad_servitor_veteran = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	tau_builder_squad = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR_CONSTRUCT, TimeMustBeIdleFor = 15, Radius = 40 },
+	tyranids_squad_builder = { Behaviour = eIDLE_STATE_BEHAVIOUR_CONSTRUCT, TimeMustBeIdleFor = 10, Radius = 40 },
+	
+	--[[Cannibalize]]
 	dark_eldar_squad_mandrake = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 35 },
 	tau_kroot_carnivore_squad = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 15 },
 	necron_flayed_one_squad = { Behaviour = eIDLE_STATE_BEHAVIOUR_CANNIBALIZE, TimeMustBeIdleFor = 15 },
+	
+	ork_squad_mek_boy = { Behaviour = eIDLE_STATE_BEHAVIOUR_REPAIR, TimeMustBeIdleFor = 25, Radius = 25 },
 	
 	chaos_squad_cultist = { Behaviour = eIDLE_STATE_BEHAVIOUR_WORSHIP_CHAOS, TimeMustBeIdleFor = 2, AbilityCaster = "" }
 }
