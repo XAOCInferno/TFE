08bcff08cd6060f78addd02c6e376cbc67e6dd74
BenB <TestXBot@gmail.com>
2025-01-02 20:34:06 +0000

    [NIDS] Nerf some melee weapons

--- Data/attrib/weapon/tyranids_claw_spinegaunt.lua
+++ Data/attrib/weapon/tyranids_claw_spinegaunt.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\melee_basic.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 6
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 4.3
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 5.6
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 4.1
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 0.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 2
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry05"] = Reference([[tables\hit_event_entry.lua]])
