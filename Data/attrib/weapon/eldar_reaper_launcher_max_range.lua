afbef690f968da76396ce9965022b2362658decd
BenB <TestXBot@gmail.com>
2025-01-12 16:35:45 +0000

    [ELDAR] DR buffs

--- Data/attrib/weapon/eldar_reaper_launcher_max_range.lua
+++ Data/attrib/weapon/eldar_reaper_launcher_max_range.lua
@@ -1,3 +1,3 @@
 GameData = Inherit([[weapon\eldar_reaper_launcher_base.lua]])
-GameData["accuracy"] = GameData["accuracy"] / 2
+GameData["accuracy"] = GameData["accuracy"] / 1.75
 GameData["max_range"] = GameData["max_range"] + 4
--- Data/attrib/weapon/eldar_reaper_launcher_min_range.lua
+++ Data/attrib/weapon/eldar_reaper_launcher_min_range.lua
@@ -1,2 +1,3 @@
 GameData = Inherit([[weapon\eldar_reaper_launcher_base.lua]])
+GameData["accuracy"] = GameData["accuracy"] / 2
 GameData["max_range"] = GameData["max_range"] / 5
