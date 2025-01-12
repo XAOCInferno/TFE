ac519410185d8f115040e747d7f559443ae6f6c1
omar\Col <omarsarakbi206@gmail.com>
2025-01-12 16:46:46 +0200

    [CHAOS] Havocs Lascannon Adjustments

--- Data/attrib/weapon/chaos_lascannon_havoc.lua
+++ Data/attrib/weapon/chaos_lascannon_havoc.lua
@@ -1,5 +1,5 @@
 GameData = Inherit([[]])
-GameData["accuracy"] = 0.8499999762
+GameData["accuracy"] = 0.75
 GameData["accuracy_reduction_when_moving"] = 1
 GameData["area_effect"] = Reference([[tables\area_effect_table.lua]])
 GameData["area_effect"]["area_effect_information"]["remove_modifiers_with_source_entity"] = true 
@@ -20,8 +20,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_13"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Lascannon.lua]])
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 252.65
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 217.5
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 692.65
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 580.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 20
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 10
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\chaos_lascannon_hit]]
