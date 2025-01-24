3edaaa2e7177df2fa0688a5f0c8ad5c3189e7980
BenB <TestXBot@gmail.com>
2025-01-24 21:33:57 +0000

    [CODE] Initial implementation of class deconstructors, SquadData, EntityData and child classes to reduce memory leakage

---
+++ Data/scar/ListAllClasses.py
@@ -0,0 +1,30 @@
+import os
+
+def GetFileNames(path, extension, doPrint):
+    acceptedFiles = []
+    files = []
+    
+    for path, subdirs, files in os.walk(path):
+        
+        if path[-7:] == "Classes":
+            for name in files:
+                
+                files.clear()
+                files = os.listdir(path)
+
+                for entity in files:
+                  split = os.path.splitext(entity)
+                  
+                  if split[1] == "." + extension:
+                      print(path + " ; " + split[0])
+                      acceptedFiles.append(split[0])
+
+    if doPrint:   
+        for acceptedFile in acceptedFiles: 
+            print(acceptedFile)
+
+    return acceptedFiles
+
+if __name__ == "__main__":
+    GetFileNames("C:/Program Files (x86)/Steam/steamapps/common/Dawn of War Soulstorm/TFE/Data/Scar", "scar", True)
+    input("Press any key to end...")
