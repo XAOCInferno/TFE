a332e211213402754a560e6f72516bcc7492c57b
BenB <TestXBot@gmail.com>
2025-01-31 20:52:52 +0000

    [DE] Hellion damage rebalances

--- TFE/Data/attrib/weapon/dark_eldar_tormentor_helm_hellion_succubus.lua
+++ TFE/Data/attrib/weapon/dark_eldar_tormentor_helm_hellion_succubus.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Psychic_Light.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 12
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 9
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 9
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 7
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 1
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 1.049999952
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\Blood_Splatter_Impact_S]]
