6aa6a6b7ac3c764cb8a9301d4c4bae3871b7e14a
BenB <TestXBot@gmail.com>
2025-02-07 20:49:13 +0000

    [CSM, SM] Predator rebalances

--- TFE/Data/attrib/weapon/space_marine_heavy_bolter_tank_left.lua
+++ TFE/Data/attrib/weapon/space_marine_heavy_bolter_tank_left.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Bolt_Heavy.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 77
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 64
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 60
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 44
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 2
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 12
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\Impact_bolter_hvy_blood_only]]
