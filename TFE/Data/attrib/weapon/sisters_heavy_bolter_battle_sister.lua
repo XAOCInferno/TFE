64e94a90ad52c28402c2e7dd481de5b9402f1238
omar\Col <omarsarakbi206@gmail.com>
2025-03-23 13:52:13 +0200

    [SISTERS] Changes to heavy bolter and early game

--- TFE/Data/attrib/weapon/sisters_heavy_bolter_battle_sister.lua
+++ TFE/Data/attrib/weapon/sisters_heavy_bolter_battle_sister.lua
@@ -22,8 +22,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"][
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Bolt_Heavy.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["damage_reduction_when_moving"] = 1
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 35
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 31
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 32
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 28
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 2
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 10
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\Impact_bolter_hvy_blood_only]]
