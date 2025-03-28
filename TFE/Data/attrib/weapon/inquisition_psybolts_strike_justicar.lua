7b209b2dfaa884efdae8bed98c11719a18743198
BenB <TestXBot@gmail.com>
2025-03-28 00:30:48 +0000

    [OM] Mild buff to strike squad ranged damage

--- TFE/Data/attrib/weapon/inquisition_psybolts_strike_justicar.lua
+++ TFE/Data/attrib/weapon/inquisition_psybolts_strike_justicar.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Psychic_Light.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 35
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 20
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 36.5
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 22
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 2.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 4
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[hit_events/psychic_hit_short]]
