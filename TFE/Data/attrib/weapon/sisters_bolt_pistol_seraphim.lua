b158636339376c5a9435eaa5f48e884c2b108427
omar\Col <omarsarakbi206@gmail.com>
2025-03-29 17:02:06 +0300

    [SISTERS] Seraphims damage nerf

--- TFE/Data/attrib/weapon/sisters_bolt_pistol_seraphim.lua
+++ TFE/Data/attrib/weapon/sisters_bolt_pistol_seraphim.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Bolt_Pistol.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 32
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 23
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 28
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 20
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 2.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 4.5
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Unit_Impact_events\Blood_Splatter_Impact_S]]
