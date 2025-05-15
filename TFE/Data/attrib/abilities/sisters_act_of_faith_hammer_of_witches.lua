325435b31c9bd3c19de9916630d049fd31e8c1e4
LeEspresso <TestXBot@gmail.com>
2025-05-15 21:35:27 +0100

    [SISTERS] Increase faith cost of hammer of witches

--- TFE/Data/attrib/abilities/sisters_act_of_faith_hammer_of_witches.lua
+++ TFE/Data/attrib/abilities/sisters_act_of_faith_hammer_of_witches.lua
@@ -171,7 +171,7 @@ GameData["execute_table"]["weapon_damage_table"]["modifiers"]["modifier_09"]["mo
 GameData["execute_table"]["weapon_damage_table"]["modifiers"]["modifier_10"] = Reference([[tables\time_modify_entry.lua]])
 GameData["execute_table"]["weapon_damage_table"]["modifiers"]["modifier_10"]["modifier"]["shield_of_faith"] = false
 GameData["fire_cost"] = Reference([[tables\cost_table.lua]])
-GameData["fire_cost"]["faith"] = 5
+GameData["fire_cost"]["faith"] = 10
 GameData["friendly_opacity_while_active"] = 1
 GameData["initial_delay_time"] = 2
 GameData["looping_event_name"] = [[]]
