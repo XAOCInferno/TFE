636fe8d0fc30529a98b7d3a42959f4a38755d5b5
LeEspresso <TestXBot@gmail.com>
2025-05-25 14:20:13 +0100

    [GUARD] Nerf senti lascannon dmg

--- TFE/Data/attrib/weapon/guard_lascannon_sentinel.lua
+++ TFE/Data/attrib/weapon/guard_lascannon_sentinel.lua
@@ -21,8 +21,8 @@ GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_14"][
 GameData["area_effect"]["area_effect_information"]["target_filter"]["entry_15"]["screen_name_id"] = [[$90100]] -- Armour
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["armour_piercing_types"] = Reference([[tables\piercings\Ranged_Lascannon.lua]])
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["life_leech_damage"] = 0
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 570
-GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 480
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["max_damage"] = 500
+GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage"] = 420
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["min_damage_value"] = 10.5
 GameData["area_effect"]["weapon_damage"]["armour_damage"]["morale_damage"] = 10.05000019
 GameData["area_effect"]["weapon_damage"]["hit_events"]["entry01"]["event"] = [[Guard\Weapons\Lasgun_Impact_Flesh]]
